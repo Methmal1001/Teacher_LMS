ce618a01f713259448174f3eeef26e336b93e87a
Methmal1001 <hau.methmal1@gmail.com>
2026-01-07 10:04:51 +0530

    Initial commit of Nuxt.js project

---
+++ lmsSystem/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="font-bold text-blue-300">
+    <h1>Welcome to the Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
