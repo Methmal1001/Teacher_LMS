6ce4a2a0e56e538c796fc7db48ff10d8632025f6
Methmal1001 <hau.methmal1@gmail.com>
2026-01-09 13:57:01 +0530

    Initial commit of Nuxt project

---
+++ components/sidebarleft.vue
@@ -0,0 +1,37 @@
+<template>
+  <aside class="w-85 bg-gray-800 text-gray-100 p-4 flex flex-col space-y-4">
+    <!-- <nav>
+      <ul class="space-y-3">
+        <li class="hover:bg-gray-700 px-3 py-2 rounded cursor-pointer">Dashboard</li>
+        <li class="hover:bg-gray-700 px-3 py-2 rounded cursor-pointer">My Courses</li>
+      </ul>
+    </nav> -->
+    <div class="bg-gray-200 border-lg p-5 text-gray-600 rounded-lg">
+      <div class="font-bold">Dashboard</div>
+      <div class="py-2">My Courses</div>
+      <div class="ml-5">
+        <div class="py-2 hover:text-blue-500 cursor-pointer">
+          <button class="bg-blue-300 px-2 py-1 rounded hover:text-white hover:bg-blue-700 transition duration-200 w-64 text-left break-words">
+            <a href="#">ABC-1019-Introduction to ABC</a>
+          </button>
+        </div>
+        <div class="py-2 hover:text-blue-500">
+          <button class="bg-blue-300 px-2 py-1 rounded hover:text-white hover:bg-blue-700 transition duration-200 w-64 text-left break-words">
+            <a href="#">EEI-1909-Introduction to Java</a>
+          </button>
+        </div>
+        <div class="py-2 hover:text-blue-500">
+          <button class="bg-blue-300 px-2 py-1 rounded hover:text-white hover:bg-blue-700 transition duration-200 w-64 text-left break-words">
+            <a href="#">AMD-5680-Introduction to Python</a>
+          </button>
+        </div>
+        <div class="py-2 hover:text-blue-500">
+          <button class="bg-blue-300 px-2 py-1 rounded hover:text-white hover:bg-blue-700 transition duration-200 w-64 text-left break-words">
+            <a href="#">DDK-0097-Mathematical Operations</a>
+          </button>
+        </div>
+      </div>
+    </div>
+
+  </aside>
+</template>
